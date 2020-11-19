c0122b9bbc9052e1bb88be7ad682655936a86554
cdhahaha <cdhahaha@163.com>
2020-11-19 15:43:42 +0800

    修改bug

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2020-11-18 09:24:29
+ * @LastEditTime: 2020-11-19 15:42:50
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \web\src\components\MessageBox\index.js
+ */
 import MessageBox from './src/main.js'
 export default MessageBox
